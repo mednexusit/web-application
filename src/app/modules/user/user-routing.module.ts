586be1f926af5be7074a3a8107093834f00574e5
Nadim Shah <nadim.shah.official@gmail.com>
2024-05-27 20:13:29 +0530

    Added the changes

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ConferenceComponent } from './components/conference/conference.component';
+import { AddEventComponent } from './components/add-event/add-event.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { SupportComponent } from './components/support/support.component';
+import { UserLoginComponent } from './components/user-login/user-login.component';
 
-const routes: Routes = [{path:'',component:HomeComponent}];
+const routes: Routes = [{path:'',component:HomeComponent,children:[
+  {path:'aboutus',component:AboutUsComponent},
+  {path:'conferences',component:ConferenceComponent},
+  {path:'addevent',component:AddEventComponent},
+  {path:'features',component:FeaturesComponent},
+  {path:'support',component:SupportComponent},
+  {path:'userlogin',component:UserLoginComponent},
+]}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -3,15 +3,33 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UserLoginComponent } from './components/user-login/user-login.component';
+import { SupportComponent } from './components/support/support.component';
+import { AddEventComponent } from './components/add-event/add-event.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { ConferenceComponent } from './components/conference/conference.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { SharedModuleModule } from '../../common/shared-module/shared-module.module';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    UserLoginComponent,
+    SupportComponent,
+    AddEventComponent,
+    FeaturesComponent,
+    ConferenceComponent,
+    AboutUsComponent
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    SharedModuleModule
   ]
 })
 export class UserModule { }
