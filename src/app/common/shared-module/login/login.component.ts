1ea4f025208c8bc9909cd8e16129ca740dcc4c55
Nadim Shah <nadim.shah.official@gmail.com>
2024-05-29 10:13:41 +0530

    Added the login page for user

--- src/app/common/shared-module/login/login.component.ts
+++ src/app/common/shared-module/login/login.component.ts
@@ -5,22 +5,26 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent implements OnInit {
-  loginForm:any=FormGroup;
-  constructor(private fb:FormBuilder, public route:Router){
-
-
-  }
+  loginForm: any = FormGroup;
+  userLoginForm:any=FormGroup;
+  constructor(private fb: FormBuilder, public route: Router) {}
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      username:['',Validators.required],
-      password:['',Validators.required]
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+    this.userLoginForm = this.fb.group({
+      mobile:['',Validators.compose([Validators.required,Validators.minLength(10),Validators.maxLength(10),Validators.pattern(/^\d{10}$/)])]
     })
   }
-  loginAdmin(data:any){
+  loginAdmin(data: any) {
     console.log(data);
+  }
+  userLogin(data:any){
+    console.log(data)
   }
 }
