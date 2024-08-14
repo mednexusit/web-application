ef293361f8c449819232a0035a826a23c75fbc09
Nadim Shah <nadim.shah.official@gmail.com>
2024-08-14 17:21:52 +0530

    added the changes

--- src/app/modules/admin/components/addconference/addconference.component.ts
+++ src/app/modules/admin/components/addconference/addconference.component.ts
@@ -6,51 +6,49 @@ import { Editor, schema, toHTML, Toolbar } from 'ngx-editor';
 @Component({
   selector: 'app-addconference',
   templateUrl: './addconference.component.html',
-  styleUrl: './addconference.component.scss'
+  styleUrl: './addconference.component.scss',
 })
-export class AddconferenceComponent implements OnInit,OnDestroy {
-  aboutConferenceFG:any=FormGroup;
-  scheduleFG:any=FormGroup;
-  speakerFG:any=FormGroup;
-  locationFG:any=FormGroup;
-  vendorData:any;
+export class AddconferenceComponent implements OnInit, OnDestroy {
+  aboutConferenceFG: any = FormGroup;
+  scheduleFG: any = FormGroup;
+  speakerFG: any = FormGroup;
+  locationFG: any = FormGroup;
+  vendorData: any;
   currentStep = 1;
-  editor:any;
-  html:any;
+  editor: any;
+  html: any;
   toolbar: Toolbar = [
     ['bold', 'italic'],
-    ['undo','redo'],
+    ['undo', 'redo'],
     ['underline', 'strike'],
     ['code', 'blockquote'],
     ['ordered_list', 'bullet_list'],
     [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
-    ['link','image'],
+    ['link', 'image'],
     ['text_color', 'background_color'],
     ['align_left', 'align_center', 'align_right', 'align_justify'],
   ];
 
-  constructor(private route:ActivatedRoute, private fb:FormBuilder){
-
-  }
+  constructor(private route: ActivatedRoute, private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe((data:any)=>{
+    this.route.queryParams.subscribe((data: any) => {
       console.log(data);
-      this.vendorData=data;
-    })
+      this.vendorData = data;
+    });
     this.editor = new Editor({});
     this.aboutConferenceFG = this.fb.group({
-      aboutconferencedata:['',Validators.required]
-    })
+      aboutconferencedata: ['', Validators.required],
+    });
     this.scheduleFG = this.fb.group({
-      scheduledata:['',Validators.required]
-    })
+      scheduledata: ['', Validators.required],
+    });
     this.speakerFG = this.fb.group({
-      speakerdata:['',Validators.required]
-    })
+      speakerdata: ['', Validators.required],
+    });
     this.locationFG = this.fb.group({
-      locationdata:['',Validators.required]
-    })
+      locationdata: ['', Validators.required],
+    });
   }
   changeCurrentStep(data: number) {
     this.currentStep = data;
@@ -68,10 +66,13 @@ export class AddconferenceComponent implements OnInit,OnDestroy {
     }
   }
 
-  onSubmit(){
-
-    console.log(this.aboutConferenceFG.value, this.scheduleFG.value, this.speakerFG.value, this.locationFG.value)
-
+  onSubmit() {
+    console.log(
+      this.aboutConferenceFG.value,
+      this.scheduleFG.value,
+      this.speakerFG.value,
+      this.locationFG.value
+    );
   }
   ngOnDestroy(): void {
     this.editor.destroy();
