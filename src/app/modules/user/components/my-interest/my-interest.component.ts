39211ad1cc045126eb2b80261afdede4f6191066
sumanth0222 <151733935+sumanth0222@users.noreply.github.com>
2024-09-17 11:42:47 +0530

    responsive changes

--- src/app/modules/user/components/my-interest/my-interest.component.ts
+++ src/app/modules/user/components/my-interest/my-interest.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialogRef, MatDialog } from '@angular/material/dialog';
+import { MatDrawerMode } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-my-interest',
@@ -6,5 +8,74 @@ import { Component } from '@angular/core';
   styleUrl: './my-interest.component.scss'
 })
 export class MyInterestComponent {
+
+
+  isSidebarOpen = true; // Sidebar initially open
+  sidenavMode: MatDrawerMode = 'side'; // Correctly typing the sidenavMode as MatDrawerMode
+  selectedValueTab1: string = 'all' ;
+  selectedValueTab2: string = 'aboutConference';
+
+
+  private dialogRef: MatDialogRef<any> | null = null;
+
+
+  onTabChange(event: any) {
+    if (event.index === 0) {
+      this.selectedValueTab1 = 'all';  // Reset Tab 1
+    } else if (event.index === 1) {
+      this.selectedValueTab2 = 'aboutConference';  // Reset Tab 2
+    }
+  }
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+
+
+  isDarkTheme = false;
+  logoSrc: string;
+  userLogoSrc:string;
+  toggleLogoSrc:string;
+  isLoggedInUser:boolean=false;
+
+
+  constructor(   public dialog: MatDialog) {}
+  ngOnInit(): void {
+   this.isLoggedInUser =  localStorage.getItem('LoggedInUser') !== null;
+  }
+
+
+
+  openPersonalDetails() {
+    if (this.dialogRef) {
+      this.dialogRef.close(); // Close any previously opened modal
+    }
+
+    this.dialogRef = this.dialog.open(MyInterestComponent, {
+      width: '400px',
+      height: '100%',
+      position: { top: '0', right: '0' },
+      panelClass: 'custom-modalbox',
+      disableClose: false, // Allow outside click to close
+      backdropClass: 'modal-backdrop',
+    });
+
+    document.body.style.overflow = 'hidden'; // Disable body scroll when modal is open
+
+    this.dialogRef.afterClosed().subscribe(() => {
+      document.body.style.overflow = ''; // Restore scroll after closing modal
+      this.dialogRef = null; // Clear the reference when closed
+    });
+  }
+
+  closeModal() {
+    if (this.dialogRef) {
+      this.dialogRef.close(); // Close the dialog when close button is clicked
+    }
+  }
+
+
+
 
 }
