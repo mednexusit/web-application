e7d744b0c44fdab1504c050972f1ed80e1d3f79f
Nadim Shah <nadim.shah.official@gmail.com>
2024-11-28 16:22:12 +0530

    Added the changes

--- src/app/modules/user/components/conferencedetails/conferencedetails.component.ts
+++ src/app/modules/user/components/conferencedetails/conferencedetails.component.ts
@@ -7,7 +7,7 @@ import { UserService } from '../../services/user.service';
 import { ToastrService } from 'ngx-toastr';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
+declare var Razorpay: any;
 
 @Component({
   selector: 'app-conferencedetails',
@@ -21,54 +21,59 @@ export class ConferencedetailsComponent implements OnInit {
   sanitizedHTMLAboutSpeaker: SafeHtml;
   sanitizedHTMLAboutSchedule: SafeHtml;
   sanitizedHTMLAboutLocation: SafeHtml;
-  sanitizedHTMLAboutPersonal:SafeHtml;
-  participantsData:any=[];
-  userData:any;
-  isOpenAddMemeber:boolean=false;
-  editData:any;
+  sanitizedHTMLAboutPersonal: SafeHtml;
+  participantsData: any = [];
+  userData: any;
+  userDetailsData:any;
+  isOpenAddMemeber: boolean = false;
+  editData: any;
 
-  addMemberForm:any=FormGroup;
-  addEditLabel:string='Add';
-  gender:any=[{label:'Male',value:1},{label:"Female",value:2}];
+  addMemberForm: any = FormGroup;
+  addEditLabel: string = 'Add';
+  gender: any = [
+    { label: 'Male', value: 1 },
+    { label: 'Female', value: 2 },
+  ];
   constructor(
-    private toastr:ToastrService,
+    private toastr: ToastrService,
     private ref: MatDialogRef<ConferencedetailsComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private sanitizer: DomSanitizer,
-    private userServ:UserService,
-    private fb:FormBuilder,
-    private router:Router
+    private userServ: UserService,
+    private fb: FormBuilder,
+    private router: Router
   ) {
     this.conferenceData = data;
   }
 
-
   ngOnInit(): void {
+
     this.userData = sessionStorage.getItem('userData');
     this.userData = JSON.parse(this.userData);
+    this.getUserDetails(this.userData);
     //this.fetchBookingConfirmationList();
     // this.sanitizedHtml = this.sanitizer.bypassSecurityTrustHtml(
     //   this.newsFeedDetails[0]?.details
     // );
     this.addMemberForm = this.fb.group({
-      "user_uuid":['',Validators.required],
-      "name":['',Validators.required],
-      "email":['',Validators.compose([Validators.required,Validators.email])],
-      "mobile_number":['',Validators.required],
-      "age":['',Validators.required],
-      "gender":['',Validators.required],
-      "mbbs":[''],
-      "postgraduation":[''],
-      "superspecialty":[''],
-      "current_position":[''],
-      "exporstudiing_year":[''],
-      "current_working_or_studying":[''],
-      "council_state":[''],
-      "council_reg":[''],
-      "yearofstudying":[''],
-      "yearofpracticing":[''],
-      "medRegisterNum":['']
-    })
+      user_uuid: ['', Validators.required],
+      name: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      mobile_number: ['', Validators.required],
+      age: ['', Validators.required],
+      gender: ['', Validators.required],
+      mbbs: [''],
+      postgraduation: [''],
+      superspecialty: [''],
+      current_position: [''],
+      exporstudiing_year: [''],
+      current_working_or_studying: [''],
+      council_state: [''],
+      council_reg: [''],
+      yearofstudying: [''],
+      yearofpracticing: [''],
+      medRegisterNum: [''],
+    });
 
     this.getParticipants();
     this.sanitizedHtmlAboutConference = this.sanitizer.bypassSecurityTrustHtml(
@@ -85,10 +90,10 @@ export class ConferencedetailsComponent implements OnInit {
     );
     this.sanitizedHTMLAboutPersonal = this.sanitizer.bypassSecurityTrustHtml(
       this.conferenceData.about_personal
-    )
+    );
   }
-  goBack(){
-   // this.userServ.goBack();
+  goBack() {
+    // this.userServ.goBack();
     this.ref.close();
   }
   openPersonalDetails() {}
@@ -99,54 +104,74 @@ export class ConferencedetailsComponent implements OnInit {
     this.selectedLabel = data.tab.textLabel;
   }
 
-
-
-
-  deleteParticipant(data:any){
-    let dataToPass={
-      "id": data.id,
-      "user_uuid": data.user_uuid
+  getUserDetails(data: any) {
+    if (data?.userid) {
+      let dataToPass = {
+        user_id: data.userid,
+      };
+      this.userServ.getUserDetails(dataToPass).subscribe({
+        next: (data: any) => {
+          this.userDetailsData = data.responseContents;
+          this.userDetailsData = this.userDetailsData[0];
+          console.log(this.userDetailsData)
+          if (this.userDetailsData[0]?.course) {
+            // this.getUserSpecialities();
+          }
+        },
+        error: (err: any) => {
+          console.error(err);
+        },
+      });
     }
-    this.userServ.deleteParticipant(dataToPass).subscribe({
-      next:(data:any)=>{
-        if(data.responseContents){
-          this.getParticipants();
-          this.toastr.success('Deleted Successfully','',{timeOut:1000});
-        }
-      }
-      ,error:(err:any)=>{
-        console.error(err);
-        this.toastr.error('Server Error','Failed to delete',{timeOut:1000});
-      }
-    })
   }
 
-  getParticipants(){
-    let dataToPass={
-      user_uuid:this.userData.userid
-    }
+  deleteParticipant(data: any) {
+    let dataToPass = {
+      id: data.id,
+      user_uuid: data.user_uuid,
+    };
+    this.userServ.deleteParticipant(dataToPass).subscribe({
+      next: (data: any) => {
+        if (data.responseContents) {
+          this.getParticipants();
+          this.toastr.success('Deleted Successfully', '', { timeOut: 1000 });
+        }
+      },
+      error: (err: any) => {
+        console.error(err);
+        this.toastr.error('Server Error', 'Failed to delete', {
+          timeOut: 1000,
+        });
+      },
+    });
+  }
+
+  getParticipants() {
+    let dataToPass = {
+      user_uuid: this.userData.userid,
+    };
     this.userServ.fetchParticipant(dataToPass).subscribe({
-      next:(data:any)=>{
-        if(data.responseContents){
+      next: (data: any) => {
+        if (data.responseContents) {
           this.participantsData = data.responseContents;
         }
-      }
-      ,error:(err:any)=>{
-        console.error(err)
-      }
-    })
+      },
+      error: (err: any) => {
+        console.error(err);
+      },
+    });
   }
-  openAddMember(type:any,data:any){
+  openAddMember(type: any, data: any) {
     this.editData = data;
-    this.isOpenAddMemeber =true;
-    if(type==='Add'){
-      this.addEditLabel ='Add';
+    this.isOpenAddMemeber = true;
+    if (type === 'Add') {
+      this.addEditLabel = 'Add';
       this.addMemberForm.reset();
     }
-    if(type==='Edit'){
-      this.addEditLabel= 'Update';
+    if (type === 'Edit') {
+      this.addEditLabel = 'Update';
       this.addMemberForm.patchValue({
-        user_uuid:data.user_uuid,
+        user_uuid: data.user_uuid,
         name: data.name,
         email: data.email,
         mobile_number: data.mobile_number,
@@ -162,74 +187,95 @@ export class ConferencedetailsComponent implements OnInit {
         council_reg: data.council_reg,
         yearofstudying: data.yearofstudying,
         yearofpracticing: data.yearofpracticing,
-        medRegisterNum: data.medRegisterNum
+        medRegisterNum: data.medRegisterNum,
       });
     }
-
   }
-  closeModals(){
-    this.isOpenAddMemeber=false;
+  closeModals() {
+    this.isOpenAddMemeber = false;
   }
-  addParticipant(data:any){
+  addParticipant(data: any) {
     data.user_uuid = this.userData.userid;
-    data.age= parseInt(data.age);
-    data.gender= parseInt(data.gender);
+    data.age = parseInt(data.age);
+    data.gender = parseInt(data.gender);
     this.userServ.addParticipant(data).subscribe({
-      next:(data:any)=>{
-        if(data.responseContents){
-          this.toastr.success('Added','',{timeOut:1000});
+      next: (data: any) => {
+        if (data.responseContents) {
+          this.toastr.success('Added', '', { timeOut: 1000 });
           this.addMemberForm.reset();
           this.getParticipants();
           this.closeModals();
-
         }
       },
-      error:(err:any)=>{
-        this.toastr.error('Failed','',{timeOut:1000});
-      }
-    })
+      error: (err: any) => {
+        this.toastr.error('Failed', '', { timeOut: 1000 });
+      },
+    });
   }
-  updateParticipant(data:any){
-    data.id=parseInt(this.editData.id)
+  updateParticipant(data: any) {
+    data.id = parseInt(this.editData.id);
     data.user_uuid = this.userData.userid;
-    data.age= parseInt(data.age);
-    data.gender= parseInt(data.gender);
+    data.age = parseInt(data.age);
+    data.gender = parseInt(data.gender);
     this.userServ.editParticipant(data).subscribe({
-      next:(data:any)=>{
-        if(data.responseContents){
-          this.toastr.success('Updated','',{timeOut:1000});
+      next: (data: any) => {
+        if (data.responseContents) {
+          this.toastr.success('Updated', '', { timeOut: 1000 });
           this.addMemberForm.reset();
           this.getParticipants();
           this.closeModals();
-
         }
       },
-      error:(err:any)=>{
-        this.toastr.error('Failed','',{timeOut:1000});
-      }
-    })
+      error: (err: any) => {
+        this.toastr.error('Failed', '', { timeOut: 1000 });
+      },
+    });
   }
-  confirmBooking(){
-    let dataToPass={
-      "user_id":this.userData.userid,
-      "conference_id":this.conferenceData.conference_id
-    }
+  confirmBooking() {
+    let dataToPass = {
+      user_id: this.userData.userid,
+      conference_id: this.conferenceData.conference_id,
+    };
     this.userServ.bookingConfirmation(dataToPass).subscribe({
-      next:(data:any)=>{
-        if(data.responseContents){
-          this.toastr.success('Booking Confirmed','',{timeOut:1000});
+      next: (data: any) => {
+        if (data.responseContents) {
+          this.toastr.success('Booking Confirmed', '', { timeOut: 1000 });
           this.closeModal();
           this.closeModals();
         }
       },
-      error:(err:any)=>{
-        this.toastr.error('Failed','',{timeOut:1000});
-      }
-    })
+      error: (err: any) => {
+        this.toastr.error('Failed', '', { timeOut: 1000 });
+      },
+    });
   }
-  goToAddMember(){
+  goToAddMember() {
     this.ref.close();
     this.router.navigate(['/dashboard/myprofile']);
+  }
+  initiatePayment() {
+    const options: any = {
+      key: 'rzp_live_62VbjqoDdEAoMw', // Replace with your Razorpay key ID
+      amount: 1,
+      currency: 'INR',
+      name: 'Your Company Name',
+      description: 'Test Transaction',
+      order_id: 'zybsbsjbsjbb',
+      handler: (response: any) => {
+        console.log('Payment Successful', response);
+        // Handle payment success logic here
+      },
+      prefill: {
+        name: this.userDetailsData.fullname,
+        email: this.userDetailsData.email,
+        contact: this.userDetailsData.user_reg_mobile,
+      },
+      theme: {
+        color: '#3399cc',
+      },
+    };
 
+    const razorpay = new Razorpay(options);
+    razorpay.open();
   }
 }
