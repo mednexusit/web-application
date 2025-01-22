888c96fe79118da60f90d0c18a6271c1326a835d
Nadim Shah <nadim.shah.official@gmail.com>
2025-01-22 15:39:52 +0530

    Added the fix

--- src/app/modules/user/components/dashboard/dashboard.component.ts
+++ src/app/modules/user/components/dashboard/dashboard.component.ts
@@ -1,6 +1,19 @@
-import { Component, Inject, ChangeDetectionStrategy, signal } from '@angular/core';
+import {
+  Component,
+  Inject,
+  ChangeDetectionStrategy,
+  signal,
+  OnInit,
+  AfterViewInit,
+  ChangeDetectorRef,
+  AfterViewChecked,
+} from '@angular/core';
 import { UserDashboardComponent } from '../user-dashboard/user-dashboard.component';
-import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  MatDialogRef,
+  MatDialog,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
 import { MatDrawerMode } from '@angular/material/sidenav';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../../auth.service';
@@ -8,14 +21,14 @@ import { ThememanageService } from '../../theme/thememanage.service';
 import { SharedService } from '../../../../shared/shared.service';
 import { UserService } from '../../services/user.service';
 import { ConferencedetailsComponent } from '../conferencedetails/conferencedetails.component';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   isSidebarOpen = true; // Sidebar initially open
   sidenavMode: MatDrawerMode = 'side'; // Correctly typing the sidenavMode as MatDrawerMode
   selectedValueTab1: string = 'all';
@@ -23,10 +36,10 @@ export class DashboardComponent {
   userData: any;
   remaindersData: any = [];
   userDetailsData: any = [];
-  userAvailableSpecialities: any = [];
+  userAvailableSpecialities: any[] = [];
   private dialogRef: MatDialogRef<any> | null = null;
   displayedItems: number = 5;
-  carnivalData:any=[];
+  carnivalData: any = [];
   step = signal(0);
 
   setStep(index: number) {
@@ -34,11 +47,11 @@ export class DashboardComponent {
   }
 
   nextStep() {
-    this.step.update(i => i + 1);
+    this.step.update((i) => i + 1);
   }
 
   prevStep() {
-    this.step.update(i => i - 1);
+    this.step.update((i) => i - 1);
   }
 
   onTabChange(event: any) {
@@ -61,7 +74,7 @@ export class DashboardComponent {
   isLoggedInUser: boolean = false;
 
   areaOfInterestData: any = [];
-  conferenceData:any;
+  conferenceData: any;
   constructor(
     private themeService: ThememanageService,
     private SharedService: SharedService,
@@ -69,24 +82,22 @@ export class DashboardComponent {
     private router: Router,
     public dialog: MatDialog,
     private userServ: UserService,
-
+    private cdr:ChangeDetectorRef
   ) {
-
     this.logoSrc = this.themeService.getLogo();
     this.userLogoSrc = this.themeService.getUserLogo();
     this.toggleLogoSrc = this.themeService.getToggleLogo();
-
   }
   ngOnInit(): void {
     this.isLoggedInUser = sessionStorage.getItem('LoggedInUser') !== null;
     this.userData = sessionStorage.getItem('userData');
     this.userData = JSON.parse(this.userData);
-    this.getUserDetails(this.userData);
+    this.getUserDetailsData(this.userData);
     this.fetchAreaOfInterest(this.userData);
-    this.fetchAllRemainders();
+    this.fetchAllRemainders(this.userData);
     this.fetchConferences();
-    //this.getUserSpecialities();
   }
+
   logoutUser() {
     this.authServ.logoutUser();
     this.router.navigate(['']);
@@ -149,10 +160,10 @@ export class DashboardComponent {
       // width: '400px',
       // height: '100%',
       // position: { top: '0', right: '0' },
-      height: "calc(100%)",
-      width: "calc(100%)",
-      maxWidth: "100%",
-      maxHeight: "100%",
+      height: 'calc(100%)',
+      width: 'calc(100%)',
+      maxWidth: '100%',
+      maxHeight: '100%',
       // panelClass: 'custom-modalbox',
       disableClose: false, // Allow outside click to close
       // backdropClass: 'modal-backdrop',
@@ -172,16 +183,21 @@ export class DashboardComponent {
     }
   }
 
-  getUserDetails(data: any) {
+  getUserDetailsData(data: any) {
     if (data?.userid) {
       let dataToPass = {
         user_id: data.userid,
       };
       this.userServ.getUserDetails(dataToPass).subscribe({
-        next:async (data: any) => {
-          this.userDetailsData = await data.responseContents;
-          if (this.userDetailsData[0]?.course) {
-            this.getUserSpecialities();
+        next:  (data: any) => {
+          this.userDetailsData =  [...data.responseContents];
+          if (
+            this.userDetailsData &&
+            this.userDetailsData.length > 0 &&
+            this.userDetailsData[0]?.course
+          ) {
+            this.getUserSpecialities(this.userDetailsData[0]);
+            // this.getUserDataAndSpecialities();
           }
         },
         error: (err: any) => {
@@ -190,60 +206,63 @@ export class DashboardComponent {
       });
     }
   }
-  getUserSpecialities() {
+  getUserSpecialities(data: any) {
     let dataToPass = {
-      course_id: this.userDetailsData[0]?.course,
+      course_id: data?.course,
       sub_courses_sub_list: this.userDetailsData[0]?.sub_course_list,
     };
     this.userServ.getSpecialitiesAvailable(dataToPass).subscribe({
       next: (data: any) => {
-        this.userAvailableSpecialities = data.responseContents;
+        this.userAvailableSpecialities = [...data.responseContents];
+        this.cdr.detectChanges();
       },
       error: (err: any) => {
         console.error(err);
       },
     });
   }
-  goToConferences(data:any){
+  goToConferences(data: any) {
     this.SharedService.sendSubjectData(data);
-    this.router.navigate(['dashboard/home/conferences-list',0])
+    this.router.navigate(['dashboard/home/conferences-list', 0]);
   }
 
-  openModal(data:any){
-    this.dialog.open(ConferencedetailsComponent,{
-      data:data,
+  openModal(data: any) {
+    this.dialog.open(ConferencedetailsComponent, {
+      data: data,
       // height:'500px'
-      height: "calc(100%)",
-      width: "calc(100%)",
-      maxWidth: "100%",
-      maxHeight: "100%"
-    })
+      height: 'calc(100%)',
+      width: 'calc(100%)',
+      maxWidth: '100%',
+      maxHeight: '100%',
+    });
   }
-  goToConference(data:any){
-    this.router.navigate(['dashboard/home/conferences-list',data.subject_uuid])
+  goToConference(data: any) {
+    this.router.navigate([
+      'dashboard/home/conferences-list',
+      data.subject_uuid,
+    ]);
   }
 
   goToAreaOfInterest() {
     this.router.navigate(['dashboard/areaofinterest']);
   }
 
-  fetchConferences(){
+
+  fetchConferences() {
     this.userServ.getAllSearchConferences().subscribe({
-      next:(data:any)=>{
-        if(data){
-          this.carnivalData = data.responseContents;
-          this.carnivalData = this.carnivalData.slice(-5);
-          this.carnivalData.forEach((item:any) => {
-              if(item.price== "null"){
-                item.price=0;
-              }
+      next: (data: any) => {
+        if (data) {
+          this.carnivalData = [...data.responseContents];
+          this.carnivalData = this.carnivalData.slice(-5)
+          this.carnivalData.forEach((item: any) => {
+            item.price = item.price === 'null' ? 0 : item.price;
           });
+        } else {
+          console.warn('No conference data found.');
         }
       },
-      error:(err:any)=>{
-        console.error(err);
-      }
-    })
+      error: (err: any) => console.error(err),
+    });
   }
   get limitedAreaOfInterestData() {
     return this.areaOfInterestData.slice(0, this.displayedItems);
@@ -272,8 +291,16 @@ export class DashboardComponent {
     const today = new Date();
 
     // Convert both dates to UTC midnight to remove timezone discrepancies
-    const utcProposalDate = Date.UTC(proposalDate.getUTCFullYear(), proposalDate.getUTCMonth(), proposalDate.getUTCDate());
-    const utcToday = Date.UTC(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate());
+    const utcProposalDate = Date.UTC(
+      proposalDate.getUTCFullYear(),
+      proposalDate.getUTCMonth(),
+      proposalDate.getUTCDate()
+    );
+    const utcToday = Date.UTC(
+      today.getUTCFullYear(),
+      today.getUTCMonth(),
+      today.getUTCDate()
+    );
 
     const diffInMs = utcProposalDate - utcToday;
     const diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
@@ -287,18 +314,18 @@ export class DashboardComponent {
     }
   }
 
-  fetchAllRemainders() {
+  fetchAllRemainders(data: any) {
     let dataToPass = {
-      user_id: this.userData.userid,
+      user_id: data.userid,
     };
     this.userServ.getAllRemainders(dataToPass).subscribe({
       next: (data: any) => {
-        this.remaindersData = data.responseContents;
-        this.remaindersData.forEach((item:any)=>{
-          if(item.price== 'null'){
-            item.price=0;
+        this.remaindersData = [...data.responseContents];
+        this.remaindersData.forEach((item: any) => {
+          if (item.price == 'null') {
+            item.price = 0;
           }
-        })
+        });
       },
       error: (err: any) => {
         console.error(err);
